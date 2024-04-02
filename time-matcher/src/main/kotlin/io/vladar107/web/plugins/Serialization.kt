e2477181728ed06a7f481b0f7b986a2f3fc30557
Vladislav Ramazaev <vladar107@gmail.com>
2024-04-02 12:47:37 +0200

    feat(infra): ignore
    remove idea redundant files
    fix resources problem

--- time-matcher/src/main/kotlin/io/vladar107/web/plugins/Serialization.kt
+++ time-matcher/src/main/kotlin/io/vladar107/web/plugins/Serialization.kt
@@ -1,10 +1,12 @@
 package io.vladar107.web.plugins
 
+import io.ktor.server.resources.Resources
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 
 fun Application.configureSerialization() {
+    install(Resources)
     install(ContentNegotiation) {
         json()
     }
